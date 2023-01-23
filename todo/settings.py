e5f65f05b06ef2e33761863c7c35da81f797a2ce
SamAdebisi <samadebisi12@gmail.com>
2023-01-23 22:37:25 +0100

    CORS

--- todo/settings.py
+++ todo/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
     # third party
     "rest_framework",
+    'corsheaders',
 
     # local
     'todos.apps.TodosConfig',
@@ -52,12 +53,21 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+
+    # added
+    "corsheaders.middleware.CorsMiddleware",
+
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+CORS_ALLOWED_ORIGINS = (
+    "http://localhost:3000",
+    "http://localhost:8000",
+)
 
 ROOT_URLCONF = "todo.urls"
 
