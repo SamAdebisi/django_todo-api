e45a7803eaaf1637e4e7c6912921ee0d7c2dfa99
SamAdebisi <samadebisi12@gmail.com>
2023-01-23 15:17:45 +0100

    admin

--- todos/admin.py
+++ todos/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Todo
+
+
+class TodoAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "body",
+    )
+
+
+admin.site.register(Todo, TodoAdmin)
